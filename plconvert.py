e9f2e727171c2a717041ac81545b0149bdaf8132
Ralph Gonzalez <ralphgonz@gmail.com>
2024-11-02 14:04:35 -0400

    Update plconvert.py

--- plconvert.py
+++ plconvert.py
@@ -3,17 +3,23 @@ import spotipy
 import spotipy.util as util
 import urllib.parse
 
-SCOPE = 'playlist-modify-public'
+SCOPE = 'playlist-read-private playlist-modify-private playlist-modify-public'
 OUTPUT_STEP_SIZE = 99
 
 def run(sp, username, input_playlist, output_playlist):
 
+    print("[get input playlist id]", flush=True)
     input_pl_id = get_playlist_id(sp, username, input_playlist)
+    print("[get all tracks]", flush=True)
     all_tracks = get_playlist_tracks(sp, username, input_pl_id)
+    print("[lookup tracks]", flush=True)
     track_ids = lookup_track_ids_by_album(sp, all_tracks)
 
+    print("[get output playlist id]", flush=True)
     output_pl_id = get_playlist_id(sp, username, output_playlist)
+    print("[add tracks]", flush=True)
     add_tracks_to_playlist(sp, username, output_pl_id, track_ids)
+    print("[done]", flush=True)
 
     sys.exit(0)
 
@@ -48,21 +54,28 @@ def get_playlist_id(sp, username, pl_name):
     sys.exit(1)
 
 def get_playlist_tracks(sp, username, playlist_id):
-    results = sp.user_playlist(username, playlist_id, fields="tracks,next")
-    tracks = results['tracks']
-    all_tracks = list(tracks['items'])
-    while 'next' in tracks:
-        tracks = sp.next(tracks)
-        if not tracks:
-            break
-        all_tracks.extend(tracks['items'])
+    results = sp.user_playlist_tracks(username, playlist_id)
+    tracks = results['items']
+    while results['next']:
+        results = sp.next(results)
+        tracks.extend(results['items'])
 
-    return all_tracks
+    print(f"found total {len(tracks)} tracks")
+    return tracks
 
 # Search songs on a per-album basis to reduce number of API calls
 def lookup_track_ids_by_album(sp, input_tracks):
     albums = set()
     output_track_ids = []
+    song_dictionary = {}
+    for item in input_tracks:
+        track = item['track']
+        artist = track['artists'][0]['name']
+        album = track['album']['name']
+        song = track['name']
+        key = f"{artist}|{album}|{song}"
+        song_dictionary[key] = 1
+        
     for item in input_tracks:
         track = item['track']
         album = track['album']['name']
@@ -73,20 +86,24 @@ def lookup_track_ids_by_album(sp, input_tracks):
         query = f"artist:{artist} album:{album}"
         found_tracks = sp.search(q=query, type="track", limit=20)
         if not found_tracks:
-            print(f"=== Can't match {artist}/{album}")
+            print(f"=== Can't match {artist}/{album}", flush=True)
             continue
         for found_track in found_tracks['tracks']['items']:
             id = found_track['id']
             song = found_track['name']
-            print(f"{artist}/{album}/{song.encode('utf-8')}: {id}")
+            key = f"{artist}|{album}|{song}"
+            if not key in song_dictionary:
+                continue;
+            # print(f"{artist.encode('utf-8')}/{album.encode('utf-8')}/{song.encode('utf-8')}: {id}")
             output_track_ids.append(id)
 
+    print(f"found total {len(output_track_ids)} output tracks")
     return output_track_ids
 
 def add_tracks_to_playlist(sp, username, output_playlist_id, output_track_ids):
     print(f"Adding {len(output_track_ids)} tracks to output playlist in groups of {OUTPUT_STEP_SIZE}")
     for i in range(0, len(output_track_ids), OUTPUT_STEP_SIZE):
-        print(f"Range [{i}, {i+OUTPUT_STEP_SIZE})")
+        print(f"Range [{i}, {i+OUTPUT_STEP_SIZE})", flush=True)
         sp.user_playlist_add_tracks(user=username, 
                                     playlist_id=output_playlist_id, 
                                     tracks=output_track_ids[i:i+OUTPUT_STEP_SIZE])
@@ -94,7 +111,9 @@ def add_tracks_to_playlist(sp, username, output_playlist_id, output_track_ids):
 ###############
 
 if __name__ == '__main__':
+    print("[get inputs]", flush=True)
     username, input_playlist, output_playlist = parse_args(*sys.argv)
+    print("[configure spotipy]", flush=True)
     sp = config_spotipy(username)
     run(sp, username, input_playlist, output_playlist)
 
